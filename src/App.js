1336ebd961b8d8df390bfa7659cd4696e33646cd
Mreza <seyyedi98@outlook.com>
2023-07-02 20:33:13 +0330

    travel-list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
